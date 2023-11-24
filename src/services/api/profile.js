207c0a3ddde02f3c3209c2922426cb47e57f7e88
Hanna Fjeldsaa <fjeldsaa@hotmail.com>
2023-11-24 14:43:58 +0100

    profile api call

--- src/services/api/profile.js
+++ src/services/api/profile.js
@@ -1,7 +1,8 @@
 import { API_URL_Profile } from "../../services/api/constants";
-import { authFetch } from "./auth_fetch";
+import { authFetch } from "./auth_fetch"
 
 let profileParams;
+
 
 //export async function getProfile(name) {
 //	if (!name) {
@@ -20,40 +21,37 @@ let profileParams;
  * @param {String} profileUrl This is the complete url needed for "GET" request.
  */
 export async function ProfileInfoApi(name, params) {
-  let method = "get";
+    let method = "get";
 
-  if (!name) {
-    throw new Error("Get requires a profile name!");
-  }
-  if (params === "bookings") {
-    profileParams = "?_bookings=true";
-  }
+    if (!name) {
+		throw new Error("Get requires a profile name!");
+	}
+    if(params ==="bookings") {profileParams = "?_bookings=true"} else if (params ==="venues") {profileParams = "venues"}
+    
+	const profileUrl =  API_URL_Profile + name + profileParams;
 
-  const profileUrl = API_URL_Profile + name + profileParams;
+	const response = await authFetch(profileUrl, {
+        method,});
 
-  const response = await authFetch(profileUrl, {
-    method,
-  });
-
-  return await response.json();
+	return await response.json();
 }
 
 export async function EditAvatarApi(name, media) {
-  const method = "put";
+    const method = "put";
 
-  if (!name) {
-    throw new Error("Get requires a profile name!");
-  }
+    if (!name) {
+		throw new Error("Get requires a profile name!");
+	}
+    
+	const profileUrl =  API_URL_Profile + name + "/media";
 
-  const profileUrl = API_URL_Profile + name + "/media";
+	const response = await authFetch(profileUrl, {
+        method,
+        body: JSON.stringify({ avatar: media.Avatar })});
 
-  const response = await authFetch(profileUrl, {
-    method,
-    body: JSON.stringify({ avatar: media.Avatar }),
-  });
-
-  return await response.json();
+	return await response.json();
 }
+
 
 /**
  * This async function sends an API "GET" request with a profile name.
