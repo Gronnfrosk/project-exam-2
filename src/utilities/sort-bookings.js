dc0004707ad8dc55ec5f7aed99bf2047adb42e21
Hanna Fjeldsaa <fjeldsaa@hotmail.com>
2023-11-26 20:15:25 +0100

    part of previous commit

--- src/utilities/sort-bookings.js
+++ src/utilities/sort-bookings.js
@@ -1,14 +1,17 @@
 export const sortBookings = (bookings) => {
-  const today = new Date();
-  const upcomingBookings = bookings.filter(
-    (booking) => new Date(booking.dateFrom) >= today,
-  );
-  const pastBookings = bookings.filter(
-    (booking) => new Date(booking.dateFrom) < today,
-  );
+     // Get the current date to compare with booking dates
+    const today = new Date();
 
-  upcomingBookings.sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom));
-  pastBookings.sort((a, b) => new Date(b.dateFrom) - new Date(a.dateFrom));
-
-  return [...upcomingBookings, ...pastBookings];
-};
+    // Filter upcoming bookings: those where 'dateFrom' is in the future
+    const upcomingBookings = bookings.filter(booking => new Date(booking.dateFrom) >= today);
+    // Filter past bookings: those where 'dateFrom' is in the past
+    const pastBookings = bookings.filter(booking => new Date(booking.dateFrom) < today);
+  
+    // Sort upcoming bookings by 'dateFrom' in ascending order
+    upcomingBookings.sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom));
+    // Sort past bookings by 'dateFrom' in ascending order
+    pastBookings.sort((a, b) => new Date(b.dateFrom) - new Date(a.dateFrom));
+  
+    // Concatenate upcoming and past bookings arrays and return them
+    return [...upcomingBookings, ...pastBookings];
+  };
